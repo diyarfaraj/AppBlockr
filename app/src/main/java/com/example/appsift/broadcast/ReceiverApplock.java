20f8f5718d3ecf7d0555e32300f68d0dd6fbbf1d
Diyar Faraj <diyar.faraj@gmail.com>
2021-04-29 20:35:31 +0200

    added bg seves

---
+++ app/src/main/java/com/example/appsift/broadcast/ReceiverApplock.java
@@ -0,0 +1,40 @@
+/*
+
+package com.example.appsift.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.example.screenfree.PatternLockAct;
+import com.example.screenfree.utils.Utils;
+
+public class ReceiverApplock extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Utils utils = new Utils(context);
+        String appRunning = utils.getLauncherTopApp();
+        String lastApp = utils.getLastApp();
+        if (appRunning != null) {
+            Log.d("APP RUNNINGG: ", appRunning);
+        }
+
+        if(lastApp != null){
+            Log.d("LAST APP: ", lastApp);
+        }
+
+        if(utils.isLock(appRunning)){
+            if(!appRunning.equals(utils.getLastApp())){
+                utils.clearLastApp();
+                utils.setLastApp(appRunning);
+                Intent i = new Intent(context, PatternLockAct.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                i.putExtra("broadcast_receiver", "broadcast_receiver");
+                context.startActivity(i);
+            }
+        }
+    }
+}*/
---
+++ app/src/main/java/com/example/appsift/services/ServiceApplock.java
@@ -0,0 +1,57 @@
+/*
+package com.example.appsift.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+import com.example.screenfree.broadcast.ReceiverApplock;
+
+public class ServiceApplock extends IntentService {
+
+
+    public ServiceApplock() {
+        super("ServiceApplock");
+    }
+
+    private void runApplock(){
+        long endTime = System.currentTimeMillis()+210;
+        while (System.currentTimeMillis() < endTime){
+            synchronized (this){
+                try {
+                    Intent intent = new Intent(this, ReceiverApplock.class);
+                    sendBroadcast(intent);
+                    wait(endTime-System.currentTimeMillis());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
+        runApplock();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        BackgroundManager.getInstance().init(this).startService();
+        BackgroundManager.getInstance().init(this).startAlarmManager();
+        super.onTaskRemoved(rootIntent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        BackgroundManager.getInstance().init(this).startService();
+        BackgroundManager.getInstance().init(this).startAlarmManager();
+        super.onDestroy();
+    }
+}*/
