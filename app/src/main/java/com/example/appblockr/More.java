7da1e31dfebb82f6382a0c68543dd14ea15cc476
Diyar Faraj <diyar.faraj@gmail.com>
2021-06-23 22:17:14 +0200

    added about page package

--- app/src/main/java/com/example/appblockr/More.java
+++ app/src/main/java/com/example/appblockr/More.java
@@ -3,18 +3,43 @@ package com.example.appblockr;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import mehdi.sakout.aboutpage.AboutPage;
+
 public class More extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+               /* .setImage(R.drawable.dummy_image)
+                .addItem(versionElement)
+                .addItem(adsElement)*/
+                .addGroup("Connect with us")
+                .addEmail("elmehdi.sakout@gmail.com")
+                .addWebsite("https://mehdisakout.com/")
+                .addFacebook("the.medy")
+                .addTwitter("medyo80")
+                .addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
+                .addPlayStore("com.ideashower.readitlater.pro")
+                .addGitHub("medyo")
+                .addInstagram("medyo80")
+                .create();
+
         setContentView(R.layout.activity_more);
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.info);
+
+        linearLayout.addView(aboutPage);
+
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_settings);
