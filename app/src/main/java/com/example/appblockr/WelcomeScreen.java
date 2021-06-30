d7d6991f52513c703a588ed0e321330d8a7ada45
Diyar Faraj <diyar.faraj@gmail.com>
2021-06-30 20:46:01 +0200

    added welcome screen

---
+++ app/src/main/java/com/example/appblockr/WelcomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.appblockr;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
