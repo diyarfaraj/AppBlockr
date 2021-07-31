0894936844bd6b53511d664467c8d36e9d3dd41b
Diyar Faraj <diyar.faraj@gmail.com>
2021-07-31 19:20:00 +0200

    dark theme removed

--- app/src/main/java/com/example/appblockr/SplashScreen.java
+++ app/src/main/java/com/example/appblockr/SplashScreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import gr.net.maroulis.library.EasySplashScreen;
 
@@ -13,7 +14,8 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-getSupportActionBar().hide();
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getSupportActionBar().hide();
         EasySplashScreen config = new EasySplashScreen(SplashScreen.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
